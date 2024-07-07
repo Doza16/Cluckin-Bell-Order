11f6b33ae752d7a3b1db223d1ac1667be56e27f9
keinom <keinom@keinom.kinopio>
2024-07-07 08:04:24 +0200

    Initial commit: Adding my Cluckin'Bell Order

---
+++ bigsmoke.py
@@ -0,0 +1,5 @@
+for i in range(1, 10, 1):
+	print(i, end=' Number 7, ')
+	print( end='And a Number 9 large. ')
+	print( end="One with Cheese.. and a Laaaarge Soda. ")
+print("And put some Extra Dip!!")
